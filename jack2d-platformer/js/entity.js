f2fec825268320585dc50ec6d71080d6d2c45fda
shaun <shaunwest@gmail.com>
2014-11-09 17:49:48 -0500

    ok?

--- jack2d-platformer/js/entity.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Shaun on 6/4/14.
- */
-
-jack2d('platformer.entity', ['obj', 'platformer.PhysicsObject'], function(obj, PhysicsObject) {
-  'use strict';
-
-  return obj.mixin(PhysicsObject);
-});
--- jack2d/js/interceptor/wait.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Shaun on 10/26/14.
- */
-// Wait for a promise
